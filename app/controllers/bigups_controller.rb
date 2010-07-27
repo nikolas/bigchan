8ef30b3de1d3d5eb648941b86fd0c690a465f2c9
Nik Nyby <niknyby@gmail.com>
2010-07-26 22:27:26 -0500

    hooking up bigups

--- app/controllers/bigups_controller.rb
+++ app/controllers/bigups_controller.rb
@@ -1,25 +1,21 @@
 class BigupsController < ApplicationController
-  def index
-    @bigups = Bigups.paginate(:page => params[:page])
-
-    respond_to do |format|
-      format.html
-      format.xml { render :xml => @bigups }
-    end
-  end
-
-  def new
-    @bigup = Bigup.new
-  end
+#  def index
+#    @bigups = Bigups.paginate(:page => params[:page])
+#
+  #  respond_to do |format|
+  #    format.html
+  #    format.xml { render :xml => @bigups }
+  #  end
+  #end
 
   def create
     @bigup = Bigup.new(params[:bigup])
     if @bigup.save
       flash[:success] = "congrats"
-      redirect_to 'index'
+      redirect_to root_path
     else
       flash[:error] = "uh oh"
-      render 'index'
+      render 'pages/home'
     end
   end
 
