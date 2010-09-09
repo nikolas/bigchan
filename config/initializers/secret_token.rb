2bb878236eb77e3808f0e5c4b4e03201a83cc6f1
Nik Nyby <niknyby@gmail.com>
2010-09-08 20:32:57 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bigchan::Application.config.secret_token = '92e66dfce772edad0d0fd0a78797a1d90eb2ff3a1657693fc33881facfeae66f4d0e61976ffd2aea39577e3cafb8e6fb8f81ec126dcf7a1146aa29144fdb6a05'
