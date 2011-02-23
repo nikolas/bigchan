b06f448a80a4b203bfac72160ea7079067ff595a
Nik Nyby <niknyby@gmail.com>
2011-02-23 00:39:40 -0800

    bluprint

--- app/models/bigup.rb
+++ app/models/bigup.rb
@@ -29,17 +29,11 @@ class Bigup < ActiveRecord::Base
     def transliterate(str)
       # Based on permalink_fu by Rick Olsen
 
-      # Escape str by transliterating to UTF-8 with Iconv
       s = Iconv.iconv('ascii//ignore//translit', 'utf-8', str).to_s
-      # Downcase string
       s.downcase!
-      # Remove apostrophes so isn't changes to isnt
       s.gsub!(/'/, '')
-      # Replace any non-letter or non-number character with a space
       s.gsub!(/[^A-Za-z0-9]+/, ' ')
-      # Remove spaces from beginning and end of string
       s.strip!
-      # Replace groups of spaces with single hyphen
       s.gsub!(/\ +/, '-')
 
       return s
