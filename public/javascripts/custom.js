c65065c71c2da6e5bc0a92fe85c7884f1d745053
Nik Nyby <niknyby@gmail.com>
2011-03-06 22:52:57 -0800

    removed unused things

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,11 +1,5 @@
 $(document).ready( function() {
 	$('#spool_form').draggable ({
-		scroll: false,
-		start: function(event, ui) {
-			ui.helper.addClass('being_dragged');
-		},
-		stop: function(event, ui) {
-			ui.helper.removeClass('being_dragged');
-		}
+		scroll: false
 	});
 });
