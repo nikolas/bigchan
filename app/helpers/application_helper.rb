15fe9c58333d85fbdbfe8640d821fe921ceb31c6
Nik Nyby <niknyby@gmail.com>
2011-08-07 22:22:04 -0700

    rainbow on bigup and spool pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,4 +32,9 @@ module ApplicationHelper
         :allowtransparency => true
   end
 
+	def random_color
+		colors = ['orange', 'blue', 'green', 'pink']
+		return colors[rand(colors.size)]
+	end
+
 end
--- app/helpers/spools_helper.rb
+++ app/helpers/spools_helper.rb
@@ -1,6 +1,2 @@
 module SpoolsHelper
-	def random_color
-		colors = ['orange', 'blue', 'green', 'pink']
-		return colors[rand(colors.size)]
-	end
 end
